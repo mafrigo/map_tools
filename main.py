fdd666a02b05cf1d7b6309e4b128994537b49736
frigom <matteo.frigo@tngtech.com>
2023-10-26 18:01:14 +0200

    choose which cities to show

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.plotting import make_plot, make_movie
 from src.route_reader import Route
 
-#route = Route("test/Erding_Whirlpool.gpx")
-route = Route("test/Morning_Ride.gpx")
+route = Route("test/Erding_Whirlpool.gpx")
+#route = Route("test/Morning_Ride.gpx")
 make_plot(route, zoomout_fac=0.4, add_cities_in_map=True, add_real_map=False)
-#make_movie(route, zoomout_fac=0.3, add_cities_in_map=True, add_real_map=False, frame_step=50)
+make_movie(route, zoomout_fac=0.3, add_cities_in_map=True, add_real_map=False, frame_step=50)
--- src/plotting.py
+++ src/plotting.py
@@ -7,53 +7,55 @@ import yaml
 from .route_reader import SubRoute
 import sys
 
-
 ffmpeg_path = r"C:\Users\mfrigo\ffmpeg-master-latest-win64-gpl\bin\ffmpeg.exe"
 frames_per_second = 30
 
 
-def make_plot(route, zoomout_fac=0.8, color='r', add_real_map=False, add_cities_in_map=True, output_file='output.png', fig=None):
-    if fig is None:
+def make_plot(route, zoomout_fac=0.8, route_color='r', city_color='k', add_real_map=False, add_cities_in_map=True, output_file='output.png',
+              fig=None):
+    if isinstance(route, SubRoute):  # movie
+        full_route = route.full_route
+        save_figure = False
+        show_speed = True
+        show_kms = True
+    else:  # single plot
+        full_route = route
         fig = plt.figure(figsize=(7, 9))
         save_figure = True
         show_speed = False
-    else:
-        save_figure = False
-        show_speed = True
+        show_kms = False
     gs = GridSpec(3, 1, height_ratios=[3, 1, 1])
     ax0 = fig.add_subplot(gs[0])
     ax1 = fig.add_subplot(gs[1])
     ax2 = fig.add_subplot(gs[2])
-    if isinstance(route, SubRoute):
-        full_route = route.full_route
-    else:
-        full_route = route
     lat_route_diff = abs(np.max(full_route.latitude) - np.min(full_route.latitude))
     lon_route_diff = abs(np.max(full_route.longitude) - np.min(full_route.longitude))
     map_extent = [[np.min(full_route.longitude) - lat_route_diff * zoomout_fac,
-                  np.max(full_route.longitude) + lon_route_diff * zoomout_fac],
+                   np.max(full_route.longitude) + lon_route_diff * zoomout_fac],
                   [np.min(full_route.latitude) - lat_route_diff * zoomout_fac,
                    np.max(full_route.latitude) + lat_route_diff * zoomout_fac]]
     height_extent = [np.min(full_route.altitude), np.max(full_route.altitude)]
     speed_extent = [0., np.max(np.nan_to_num(full_route.speed))]
     length_extent = [0., np.max(full_route.length)]
     if add_cities_in_map:
-        add_cities(ax0, map_extent, color=color)
+        add_cities(ax0, map_extent, color=city_color)
     if add_real_map:
         plot_europe_map(ax0, route.longitude, route.latitude, zoomout_fac=zoomout_fac)
-    ax0.plot(route.longitude, route.latitude, color=color)
+    ax0.plot(route.longitude, route.latitude, color=route_color)
+    if show_kms:
+        ax0.text(map_extent[0][0] + 0.02 * (map_extent[0][1] - map_extent[0][0]), map_extent[1][0] + 0.05 * (map_extent[1][1]- map_extent[1][0]), "%i km" % (route.length[-1]), color=route_color)
     ax0.set_xlim(map_extent[0])
     ax0.set_ylim(map_extent[1])
     ax0.set_ylabel("latitude")
     ax0.set_xlabel("longitude")
     ax0.axes.set_aspect('equal')
-    ax1.plot(route.length, route.altitude, color=color)
+    ax1.plot(route.length, route.altitude, color=route_color)
     ax1.set_xlim(length_extent)
     ax1.set_ylim(height_extent)
     ax1.set_ylabel("elevation (m)")
-    ax2.plot(route.length, route.speed, color=color)
+    ax2.plot(route.length, route.speed, color=route_color)
     if show_speed:
-        plt.text(0.02*length_extent[1], 0.05*speed_extent[1], "%.1f"%(route.speed[-1]), color=color)
+        plt.text(0.02 * length_extent[1], 0.05 * speed_extent[1], "%.1f km/h" % (route.speed[-1]), color=route_color)
     ax2.set_xlim(length_extent)
     ax2.set_ylim(speed_extent)
     ax2.set_ylabel("speed (km/h)")
@@ -63,7 +65,8 @@ def make_plot(route, zoomout_fac=0.8, color='r', add_real_map=False, add_cities_
         plt.savefig(output_file)
 
 
-def make_movie(route, zoomout_fac=0.8, color='r', add_real_map=False, add_cities_in_map=True, movie_file="movie", frame_step=1):
+def make_movie(route, zoomout_fac=0.8, color='r', add_real_map=False, add_cities_in_map=True, movie_file="movie",
+               frame_step=1):
     plt.rcParams['animation.ffmpeg_path'] = ffmpeg_path
     FFMpegWriter = mani.writers['ffmpeg']
     metadata = dict(title=movie_file, artist='Matplotlib')
@@ -76,16 +79,19 @@ def make_movie(route, zoomout_fac=0.8, color='r', add_real_map=False, add_cities
         for i in range(1, nframes, frame_step):
             subroute = SubRoute(route, i)
             make_plot(subroute,
-                      zoomout_fac=zoomout_fac, color=color, add_real_map=add_real_map, add_cities_in_map=add_cities_in_map,
-                      output_file="frame"+str(i)+".png", fig=fig)
+                      zoomout_fac=zoomout_fac, color=color, add_real_map=add_real_map,
+                      add_cities_in_map=add_cities_in_map,
+                      output_file="frame" + str(i) + ".png", fig=fig)
             writer.grab_frame()
             plt.clf()
             del subroute
             # Progress bar
             progress_counter += 1
-            progress = 100 * progress_counter / (nframes/frame_step)
+            progress = 100 * progress_counter / (nframes / frame_step)
             sys.stdout.write('\r')
-            sys.stdout.write("[{:{}}] {:.1f}%".format("="*int(progress/(100/progress_bar_length)), progress_bar_length, progress))
+            sys.stdout.write(
+                "[{:{}}] {:.1f}%".format("=" * int(progress / (100 / progress_bar_length)), progress_bar_length,
+                                         progress))
             sys.stdout.flush()
 
 
@@ -96,7 +102,8 @@ def add_cities(ax, map_extent, color='r'):
     for city in cities["all_cities"].keys():
         if city not in cities["cities_to_be_shown"]:
             continue
-        if not (map_extent[1][1] > cities["all_cities"][city]["lat"] > map_extent[1][0] and map_extent[0][1] > cities["all_cities"][city]["lon"] > map_extent[0][0]):
+        if not (map_extent[1][1] > cities["all_cities"][city]["lat"] > map_extent[1][0] and map_extent[0][1] >
+                cities["all_cities"][city]["lon"] > map_extent[0][0]):
             continue
         if cities["all_cities"][city]['size'] == 'small':
             marker = '.'
@@ -135,7 +142,7 @@ def plot_europe_map(ax, longitude, latitude, zoomout_fac=0.8):
     pixel_lat_lims = [img.shape[1] - route_max_lat_pixel, img.shape[1] - route_min_lat_pixel]
     pixel_lon_lims = [img.shape[0] - route_max_lon_pixel, img.shape[0] - route_min_lon_pixel]
     cut_image = img[pixel_lat_lims[0]:pixel_lat_lims[1], pixel_lon_lims[0]:pixel_lon_lims[1], :]
-    #print(np.min(longitude), np.max(longitude), np.min(latitude), np.max(latitude))
-    #print(route_min_lon, route_max_lon, route_min_lat, route_max_lat)
-    #print(route_min_lon_pixel, route_max_lon_pixel, route_min_lat_pixel, route_max_lat_pixel)
+    # print(np.min(longitude), np.max(longitude), np.min(latitude), np.max(latitude))
+    # print(route_min_lon, route_max_lon, route_min_lat, route_max_lat)
+    # print(route_min_lon_pixel, route_max_lon_pixel, route_min_lat_pixel, route_max_lat_pixel)
     ax.imshow(cut_image, extent=[route_min_lon, route_max_lon, route_min_lat, route_max_lat])
