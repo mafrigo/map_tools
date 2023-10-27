9973df4a2a7062407a38c677896ce8a55de80088
frigom <matteo.frigo@tngtech.com>
2023-10-27 16:34:07 +0200

    rename examples

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from src.plotting import plot_route_on_map, plot_route_on_graph, make_movie
 from src.route_reader import Route
 
-route = Route("test/Erding_Whirlpool.gpx")
-#route = Route("test/Morning_Ride.gpx")
-#route = Route("test/Munich_Budapest_1.gpx")
+route = Route("route_files/Erding_Whirlpool.gpx")
+#route = Route("route_files/Morning_Ride.gpx")
+#route = Route("route_files/Munich_Budapest_1.gpx")
 plot_route_on_map(route, zoomout_fac=0.3, output_file="map")
 plot_route_on_graph(route, zoomout_fac=0.3, output_file="graph")
 #make_movie(route, zoomout_fac=0.3, frame_step=50, output_file="movie")
